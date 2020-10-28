e2f4bb67111247f13a5a7fac4617157b85f2a420
BasitAbdul1712 <67551884+BasitAbdul1712@users.noreply.github.com>
2020-10-28 15:25:49 +0100

    simple chnage hase been made

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -12,16 +12,16 @@ const Navbar = () => {
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link mr-5 text-bold" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link mr-5 text-bold" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mr-5" href="#">Our Portfolio</a>
+                        <a class="nav-link mr-5" href="/">Our Portfolio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mr-5" href="#">Our Team</a>
+                        <a class="nav-link mr-5" href="/">Our Team</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mr-5" href="#">Contact Us</a>
+                        <a class="nav-link mr-5" href="/">Contact Us</a>
                     </li>
                     <li class="nav-item">
                     <a style= {{width: '100px'}} class="nav-link btn btn-dark text-white mr-5" href="login">Login</a>
